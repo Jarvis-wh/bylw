07e1c56c2161bad76905995eac588c061273479f
wh <191300054@smail.nju.edu.cn>
2023-05-04 08:33:00 +0800

    SL

--- DataHelper.py
+++ DataHelper.py
@@ -6,7 +6,7 @@ from numpy import random
 import tensorflow as tf
 
 class Data:
-    def __init__(self):
+    def __init__(self, batch_size, num_steps):
         # events_df = pd.read_csv('./data/retailrocket/events.csv')
         # category_tree_df = pd.read_csv('./data/retailrocket/category_tree.csv')
         # item_properties_1_df = pd.read_csv('./data/retailrocket/item_properties_part1.csv')
@@ -17,22 +17,60 @@ class Data:
         # users = users.value_counts()
         # users = users.iloc[users.values > 10].index
         # users = users.sort_values()
-        self.dataset = pd.read_csv('./data/retailrocket/SLdata.csv')
+        self.dataset = pd.read_csv('./data/retailrocket/events.csv')
+        self.df,self.item_size = self.make_df()
+        self.data_iter = self.dataset_loader(self.df, batch_size, num_steps)
 
-    def make_action_list(self):
-        group = self.dataset.groupby('visitorid',group_keys=False).groups
+    def make_df(self):
+        events_df = self.dataset.sort_values(['visitorid', 'timestamp'])
+        events_df = events_df.reset_index(drop=True)
+
+        # 筛选出其中行动序列长度大于2的
+        users = events_df['visitorid']
+        users = users.value_counts()
+        users = users.iloc[users.values > 2].index
+        users = users.sort_values()
+
+        group = events_df.groupby('visitorid',group_keys=False).groups
+        # user_list=[]
+        # action_list=[]
+        # for user in self.dataset.visitorid.unique().tolist():
+        #     item_list=[]
+        #     for item in group[user]:
+        #         item = self.dataset['itemid'][item]
+        #         item_list.append(item)
+        #     user_list.append(user)
+        #     action_list.append(item_list)
+        # data = {'user':user_list, 'actions':action_list}
+        # df = pd.DataFrame(data)
+        # return df
+    
         user_list=[]
         action_list=[]
-        for user in self.dataset.visitorid.unique().tolist():
+        all_items=[]
+        for user in events_df.visitorid.unique().tolist():
             item_list=[]
-            for item in group[user]:
-                item = self.dataset['itemid'][item]
-                item_list.append(item)
-            user_list.append(user)
-            action_list.append(item_list)
-        data = {'user':user_list, 'actions':action_list}
+            for item in group[user]:  # 将user访问过的不重复的item加入到item列表中
+                item = events_df['itemid'][item]
+                #print(item not in item_list)
+                if item not in item_list:
+                    item_list.append(item)
+            if len(item_list) > 3: # 如果访问过的不重复的物品序列长度大于k则加入到最终的数据集中
+                user_list.append(user)
+                action_list.append(item_list)
+                all_items += item_list
+        item_set = set(all_items)
+        item2id = dict(zip(item_set, range(len(item_set))))
+        id_action_list=[]
+        for lst in action_list:
+            ids=[]
+            for item in lst:
+                item = item2id[item]
+                ids.append(item)
+            id_action_list.append(ids)   
+        data = {'user':user_list, 'actions':id_action_list}
         df = pd.DataFrame(data)
-        return df
+        return df, len(item2id)
 
     def seq_data_iter_random(self, corpus, batch_size, num_steps):  #@save
         """使用随机抽样生成一个小批量子序列"""
